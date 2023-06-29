e439fa8ea3a6ccc7f585b3856bbfe6a3b7688d86
Ziyang Song <ziyang.song@gmx.de>
2023-06-29 10:18:35 +0200

    Create method to add students in a container

--- src/main/java/de/iav/javafxwithfxml/HelloController.java
+++ src/main/java/de/iav/javafxwithfxml/HelloController.java
@@ -4,18 +4,47 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloController {
+
+    private List<Student> studentList = new ArrayList<>();
 
     @FXML
     private TextField firstName_TextField;
 
     @FXML
+    private TextField lastName_TextField;
+
+    @FXML
     private TextField course_TextField;
 
     @FXML
+    private TextField email_TextField;
+
+    @FXML
     protected void onHelloButtonClick() {
-        System.out.println(firstName_TextField.getText());
-        System.out.println(course_TextField.getText());
+
+        Student studentToAdd = new Student(
+                firstName_TextField.getText(),
+                lastName_TextField.getText(),
+                email_TextField.getText(),
+                course_TextField.getText()
+                );
+
+        this.createStudent(studentToAdd);
+
+        System.out.println(this.studentList);
+
+        //System.out.println(firstName_TextField.getText());
+        //System.out.println(lastName_TextField.getText());
+        //System.out.println(course_TextField.getText());
+        //System.out.println(email_TextField.getText());
+    }
+
+    public void createStudent(Student studentToAdd){
+        this.studentList.add(studentToAdd);
     }
 
 }
---
+++ src/main/java/de/iav/javafxwithfxml/Student.java
@@ -0,0 +1,9 @@
+package de.iav.javafxwithfxml;
+
+public record Student(
+    String firstName,
+    String lastName,
+    String email,
+    String courseOfStudies
+) {
+}
